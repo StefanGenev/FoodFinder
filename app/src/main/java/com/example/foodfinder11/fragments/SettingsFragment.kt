aa0459970911b6fbecf15c17a0603a3ae6a2e86a
StefanGenev <stefangenev@abv.bg>
2024-05-21 23:41:01 +0300

    Added SettingsFragment

--- app/src/main/java/com/example/foodfinder11/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/foodfinder11/fragments/SettingsFragment.kt
@@ -1,21 +1,22 @@
 package com.example.foodfinder11.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.foodfinder11.activities.WelcomeActivity
 import com.example.foodfinder11.databinding.FragmentSettingsBinding
+import com.example.foodfinder11.utils.SessionManager
 
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
 
     override fun onCreateView(
-
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-
     ): View? {
 
         binding = FragmentSettingsBinding.inflate(inflater)
@@ -25,5 +26,25 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.signOutButton.setOnClickListener {
+            onSignOut()
+        }
+    }
+
+    private fun onSignOut() {
+
+        QuestionDialogFragment("Are you sure?",
+            "Yes",
+            "No",
+            onOkAction = { dialog, id ->
+
+                activity?.finish()
+                SessionManager.logoutOperations()
+
+                val intent = Intent(activity, WelcomeActivity::class.java)
+                startActivity(intent)
+            }
+            , onCancelAction = { dialog, id ->
+            } ).show(parentFragmentManager, "QuestionDialog")
     }
 }
